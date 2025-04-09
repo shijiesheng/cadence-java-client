adcc039dcba84bb00d23d7a05567704668762036
Shijie Sheng <liouvetren@gmail.com>
2025-04-09 09:28:48 -0700

    add serviceclient exceptions (#989)
    What changed?
    
    added all exceptions to match thrift exceptions
    added ErrorMapper which maps StatusRuntimeException to ServiceClientException
    Why?
    
    thrift exceptions are used across Java SDK. It's easier to abstract the protocol level error handling by introducing internal exception layer

---
+++ src/main/java/com/uber/cadence/serviceclient/exceptions/ServiceClientException.java
@@ -0,0 +1,37 @@
+/*
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package com.uber.cadence.serviceclient.exceptions;
+
+/**
+ * Base class for all exceptions thrown by the service client.
+ *
+ * <p>This is a catchall for all other errors.
+ */
+public class ServiceClientException extends RuntimeException {
+  ServiceClientException() {
+    super();
+  }
+
+  public ServiceClientException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public ServiceClientException(Throwable cause) {
+    super(cause);
+  }
+}
--- src/test/java/com/uber/cadence/converter/JsonDataConverterTest.java
+++ src/test/java/com/uber/cadence/converter/JsonDataConverterTest.java
@@ -245,8 +245,6 @@ public class JsonDataConverterTest {
     }
   }
 
-  // TODO flaky test in local env: expected:<class java.lang.IllegalArgumentException> but
-  // was:<class java.lang.StackOverflowError>
   @Test
   public void testException() {
     RuntimeException rootException = new RuntimeException("root exception");
@@ -263,7 +261,9 @@ public class JsonDataConverterTest {
     assertNotNull(causeFromConverted);
     assertEquals(DataConverterException.class, causeFromConverted.getClass());
     assertNotNull(causeFromConverted.getCause());
-    assertEquals(JsonIOException.class, causeFromConverted.getCause().getClass());
+    Class<?> causeClass = causeFromConverted.getCause().getClass();
+    // it depends on the gson implementation on different runtimes
+    assertTrue(causeClass == JsonIOException.class || causeClass == StackOverflowError.class);
 
     assertNotNull(causeFromConverted.getSuppressed());
     assertEquals(1, causeFromConverted.getSuppressed().length);
