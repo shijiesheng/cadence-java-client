8cd28c1a4e1cfb3cc6b6c648a95e805a5a00e10e
Shijie Sheng <liouvetren@gmail.com>
2025-06-05 09:28:58 -0700

    remove Thrift dataconverter support

--- src/main/java/com/uber/cadence/converter/JsonDataConverter.java
+++ src/main/java/com/uber/cadence/converter/JsonDataConverter.java
@@ -37,8 +37,7 @@ import org.apache.thrift.protocol.TJSONProtocol;
 
 /**
  * Implements conversion through GSON JSON processor. To extend use {@link
- * #JsonDataConverter(Function)} constructor. Thrift structures are converted using {@link
- * TJSONProtocol}. When using thrift only one argument of a method is expected.
+ * #JsonDataConverter(Function)} constructor.
  *
  * @author fateev
  */
@@ -78,9 +77,7 @@ public final class JsonDataConverter implements DataConverter {
     GsonBuilder gsonBuilder =
         new GsonBuilder()
             .serializeNulls()
-            .registerTypeAdapterFactory(new ThrowableTypeAdapterFactory())
-            .registerTypeAdapterFactory(new TBaseTypeAdapterFactory(metricsScope))
-            .registerTypeAdapterFactory(new TEnumTypeAdapterFactory());
+            .registerTypeAdapterFactory(new ThrowableTypeAdapterFactory());
     GsonBuilder intercepted = builderInterceptor.apply(gsonBuilder);
     gson = intercepted.create();
   }
