78d61a86d1ccc79401a0f2895fd18e4c3dfc06c6
Shijie Sheng <liouvetren@gmail.com>
2025-06-12 15:37:38 -0700

    change generated path and modify the script of generator

---
+++ src/gen/java/com/uber/cadence/entities/ContinueAsNewWorkflowExecutionDecisionAttributes.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * <p>Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+ * except in compliance with the License. A copy of the License is located at
+ *
+ * <p>http://aws.amazon.com/apache2.0
+ *
+ * <p>or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.uber.cadence.entities;
+
+import java.util.*;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class ContinueAsNewWorkflowExecutionDecisionAttributes {
+  private WorkflowType workflowType;
+  private TaskList taskList;
+  private byte[] input;
+  private int executionStartToCloseTimeoutSeconds;
+  private int taskStartToCloseTimeoutSeconds;
+  private int backoffStartIntervalInSeconds;
+  private RetryPolicy retryPolicy;
+  private ContinueAsNewInitiator initiator;
+  private String failureReason;
+  private byte[] failureDetails;
+  private byte[] lastCompletionResult;
+  private String cronSchedule;
+  private Header header;
+  private Memo memo;
+  private SearchAttributes searchAttributes;
+  private int jitterStartSeconds;
+}
---
+++ src/gen/java/com/uber/cadence/entities/DomainAlreadyExistsError.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * <p>Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+ * except in compliance with the License. A copy of the License is located at
+ *
+ * <p>http://aws.amazon.com/apache2.0
+ *
+ * <p>or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.uber.cadence.entities;
+
+import java.util.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+public class DomainAlreadyExistsError extends BaseError {
+
+  public DomainAlreadyExistsError() {
+    super();
+  }
+
+  public DomainAlreadyExistsError(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public DomainAlreadyExistsError(Throwable cause) {
+    super(cause);
+  }
+}
