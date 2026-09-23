24386e6c2b4f08681fd80fec5a43f0401ce763ca
Shijie Sheng <liouvetren@gmail.com>
2026-09-23 14:36:15 -0700

    revert unwanted local test
    Signed-off-by: Shijie Sheng <liouvetren@gmail.com>

--- src/test/java/com/uber/cadence/internal/tracing/StartWorkflowTest.java
+++ src/test/java/com/uber/cadence/internal/tracing/StartWorkflowTest.java
@@ -328,7 +328,7 @@ public class StartWorkflowTest {
 
   @Test
   public void testSignalStartWorkflowGRPCNoPropagation() {
-    // Assume.assumeTrue(useDockerService);
+    Assume.assumeTrue(useDockerService);
     MockTracer mockTracer = new MockTracer();
     IWorkflowService service =
         new Thrift2ProtoAdapter(
